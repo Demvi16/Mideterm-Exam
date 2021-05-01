90d3035e0867ac36bc7b413d541e6aa2f0ed864b
Demvi16 <jalopdemvi92@gmail.com>
2021-05-01 16:12:36 +0800

    modified user

--- calculator.py
+++ calculator.py
@@ -2,3 +2,22 @@ import math
 
 a = int(input('Enter value of a: '))
 b = int(input('Enter value of b: '))
+
+print("Enter your choice: ")
+print("a. Add")
+print("b. Subtract")
+print("c. Multiplication")
+print("d. Division")
+
+choice = input("Enter choice: ")
+
+if choice == 'a':
+
+elif choice == 'b':
+
+elif choice == 'c':
+
+elif choice == 'd':
+
+else:
+    print("Invalid choice")
